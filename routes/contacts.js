9ce8bef390ba068455299caac586bb7f26dae689
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2026-01-14 15:38:27 -0500

    get request added

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,25 @@ import { connectDB } from "../db.js";
 
 const router = express.Router();
 
+router.get("/", async ( req, res) => {
+    try {
+        let db = await connectDB();
+
+        let contacts = await db
+            .collection("contacts")
+            .find()
+            .sort({ createdAt: -1 })
+            .toArray();
+
+        res.status(200).json(contacts);
+
+    }catch (error) {
+        res.status(500).json( { message: "Failed to fetch contacts."});
+    }
+})
+
 router.post('/', async (req, res) => {
-    console.log("Body:", req.body);
+    // console.log("Body:", req.body);
 
     try {
         let db = await connectDB();
@@ -16,29 +33,15 @@ router.post('/', async (req, res) => {
             createdAt: new Date(),
         }
 
-        // let name = req.body.name;
-        // let email = req.body.email;
-        // let reason = req.body.reason;
-        // let createdAt = new Date();
-
-        // let data = {
-        //     name: name,
-        //     email: email,
-        //     reason: reason,
-        //     createdAt: createdAt,
-        // }
-
         await db.collection("contacts").insertOne(contact);
-        // await db.collection("contacts").insertOne(data);
 
         res.status(201).json({
             message: "Contact submitted successfully."
-            // message: "Data submitted successfully."
         })
     } catch (error) {
         console.error("Insert failed:", error);
-        res.status(500).json({ 
-            message: "Insert failed" 
+        res.status(500).json({
+            message: "Insert failed"
         });
     }
 })
